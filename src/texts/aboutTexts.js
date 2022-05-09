1d474a002ca553fe9b125a27680e73215262fd6f
matthew9655 <matthew9655@hotmail.com>
2022-05-08 20:58:57 -0400

    add vector institute

--- src/texts/aboutTexts.js
+++ src/texts/aboutTexts.js
@@ -1,6 +1,6 @@
 const texts = {
     "name": "Matthew Choi",
-    "about1": "I am an Incoming Msc CS Student at the University of Toronto.",
+    "about1": "I am an Incoming Msc CS Student at the University of Toronto and Vector Institute.",
     "about2": "I am fortunate enough to work with the Alán Aspuru-Guzik group as a Machine Learning Researcher. \
     Previously, I did a year-long internship at Modiface as an AR Application Developer. \n My research goal is to  \
     apply machine learning to other fields of Science. I also like to help others through MLOps, DevOps, and CS Education.",
