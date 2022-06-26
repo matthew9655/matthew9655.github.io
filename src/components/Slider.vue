4c8447398a8816ff0372f07d9d3f3553d6b6dd9b
matthew9655 <matthew9655@hotmail.com>
2022-06-26 15:31:00 -0400

    working slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,26 +1,28 @@
 <template>
-  <div>
-    <vue-slider 
-        v-model="value" 
-        :data="data"
-    ></vue-slider>
+  <div id="main-header">
+    <vue3-slider 
+    v-model="value"
+    width="200px"
+    track-color="#89ABE3FF"
+    color="#2c3e50"
+    step=50
+    sticky="true"></vue3-slider>
   </div>
 </template>
 
 <script>
 
-import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/default.css'
+import slider from 'vue3-slider';
 
 export default {
     name: 'Slider',
     components: {
-        VueSlider,
+        "vue3-slider": slider,
     },
     data: function() {
         return {
-            value: 'normal',
-            data: ['dark', 'normal', 'blind']
+            value: 50,
+            // data: ['dark', 'normal', 'blind']
         }
     }
 }
@@ -37,6 +39,9 @@ export default {
     height: 10%;
     margin-bottom: 60px;
     right: 5%;
+}
+#slider {
+    width: 100px;
 }
 #sub-header {
     margin: 0px 10px 0px 10px;
