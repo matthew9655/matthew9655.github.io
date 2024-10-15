903cfe75102f503fb6383a0a4febef8c208666ce
Matthew Choi <matthewchoi@MacBook-Pro-5.local>
2024-10-15 19:26:41 -0400

    swap to github actions

--- websitev2/next.config.mjs
+++ websitev2/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/matthew9655.github.io",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
