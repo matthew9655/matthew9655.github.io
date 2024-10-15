f42f2d2cb873dab31b549a3769773784063a80d2
Matthew Choi <matthewchoi@MacBook-Pro-5.local>
2024-10-15 19:38:38 -0400

    remove out from gitignore

---
+++ out/_next/static/chunks/app/others/page-c6869b05b32d4dd7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[966],{1303:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,5885,23)),Promise.resolve().then(t.t.bind(t,8541,23))},5885:function(){},8541:function(){}},function(n){n.O(0,[38,530,972,971,117,744],function(){return n(n.s=1303)}),_N_E=n.O()}]);
