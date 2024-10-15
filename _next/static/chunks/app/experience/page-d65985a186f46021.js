0cedc423f5801b73cf6e9b8ba36df9e3eb9c0694
Matthew Choi <matthewchoi@MacBook-Pro-5.local>
2024-10-15 19:20:31 -0400

    deploy

---
+++ _next/static/chunks/app/experience/page-d65985a186f46021.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[924],{9034:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,5885,23)),Promise.resolve().then(t.t.bind(t,5831,23))},5831:function(){},5885:function(){}},function(n){n.O(0,[38,920,972,971,117,744],function(){return n(n.s=9034)}),_N_E=n.O()}]);
