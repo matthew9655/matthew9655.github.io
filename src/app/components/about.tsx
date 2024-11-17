d594774d84509e79ba29f16cc247db8f1c0bf6da
Matthew Choi <matthewchoi@MacBook-Pro-5.local>
2024-11-17 18:42:28 -0500

    commit update

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -13,28 +13,35 @@ export default function About() {
                         {texts.about11} 
                         <a id="sublink" target="_blank" href={texts.link1}>Vector Institute</a>
                         {texts.about12}
-                        <a id="specialsublink" target="_blank" href={texts.link2}>University of Toronto </a>
+                        <a id="sublink" target="_blank" href={texts.link2}>University of Toronto </a>
                         {texts.about13}
                         <a id="sublink" target="_blank" href={texts.link3}>Alán Aspuru-Guzik</a>
                         {texts.about14}
+                        {texts.about21}
+                        {texts.about22}
+                        <a id="sublink" target="_blank" href={texts.link4}>Modiface</a>
+                        {texts.about23}
+                        {texts.about31}
+                        <br/>
                     </div>
                 </div>
             </div>
-            <div id="about2box">
+            {/* <div id="about2box">
                 <div>
                     {texts.about21}
                     {texts.about22}
                     <a id="sublink" target="_blank" href={texts.link4}>Modiface</a>
                     {texts.about23}
-                    <br/>
-                    <br/>
-                    {texts.about3}
+                    <br/> <br/>
+                    {texts.about31}
+                    {texts.about32}
                     <a id="sublink" target="_top" href="mailto:matt.choi531@gmail.com">matt.choi531@gmail.com</a>
                     <br/>
                 </div>
-            </div>
+            </div> */}
             <div id="socials">
-            <a id="link" target="_blank" href="https://twitter.com/matt_choi_cs">Twitter</a>
+            <a id="link" target="_blank" href="mailto:matt.choi531@gmail.com">Email</a> 
+            <a id="link" target="_blank" href="https://twitter.com/matt_choi_cs">X</a>
             <a id="link" target="_blank" href="https://github.com/matthew9655">Github</a>
             <a id="link" target="_blank" href="https://www.linkedin.com/in/matthew-choi-623aa0167/">LinkedIn</a>
             </div>
--- src/app/texts/aboutTexts.tsx
+++ src/app/texts/aboutTexts.tsx
@@ -1,17 +1,18 @@
 const texts = {
     "name": "Matthew Choi",
-    "about11": "I am currently a Machine Learning Engineer at Zapata.ai. I graduated with a MSc in Computer Science at the ",
+    "about11": "I recently graduated with a MSc in Computer Science at the ",
     // Vector Institute 
     "about12": " and the ", 
     // University of Toronto,
     "about13": " under the supervision of ",
     // Alan Aspuru Guzik
-    "about14": " .",
-    "about21": " My main research interests were applying classical generative models to quantum computing problems.",
-    "about22": " During my time in school, I had software engineering internships at Zapata.ai and ",
+    "about14": " where I applied classical machine learning to quantum computing problems. ",
+    "about21": " Previously, I was a ML Engineer at Zapata AI.",
+    "about22": " During my time in school, I had software engineering internships at Zapata AI and ",
     // Modiface
-    "about23": ". In my spare time, I enjoy skiing in the winter and taking really long walks in the summer. ",
-    "about3": " I love meeting new people so please feel free to contact me at: ",
+    "about23": ".",
+    "about31": " These days, I am interested in AI safety, LLM Compression and ML Ops. In my spare time, I enjoy skiing in the winter and taking really long walks in the summer. ",
+    "about32": " I love meeting new people so please feel free to contact me at: ",
     "link1": "https://vectorinstitute.ai/",
     "link2": "https://web.cs.toronto.edu/",
     "link3": "https://www.matter.toronto.edu/",
